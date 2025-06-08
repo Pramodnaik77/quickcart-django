5ea40b0e50131da04bf73af7a8a2ccbbda26d1d0
Pramod Naik <pramodniaik8887@gmail.com>
2025-06-08 22:50:12 +0530

    Added cloudinary

--- quickcart/settings.py
+++ quickcart/settings.py
@@ -45,7 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop.apps.ShopConfig'
+    'shop.apps.ShopConfig',
+    'cloudinary_storage',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
