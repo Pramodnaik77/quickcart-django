f4bda9172825a14cce7fd96449da1329fc67b65b
Pramod Naik <pramodniaik8887@gmail.com>
2025-06-08 21:27:03 +0530

    Added deployed url into setting.py"

--- quickcart/settings.py
+++ quickcart/settings.py
@@ -33,7 +33,7 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'quickcart-django-31fx.onrender.com/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'quickcart-django-31fx.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = [ 'https://quickcart-django-31fx.onrender.com/' ]
 # Application definition
